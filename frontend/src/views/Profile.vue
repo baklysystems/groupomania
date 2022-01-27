f7044dd38e0f644014b6977bce38f7db5c044274
amrnrd <amrmohamed257@gmail.com>
2022-01-27 23:18:28 +0200

    fix profile

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -5,47 +5,47 @@
     <b-row class="text-center justify-content-center">
       <b-col cols="12" lg="6">
         <b-card
-          class="mx-auto border-0 shadow p-lg-3 mb-2 mb-lg-5 mt-lg-3 bg-white rounded"
+            class="mx-auto border-0 shadow p-lg-3 mb-2 mb-lg-5 mt-lg-3 bg-white rounded"
         >
           <b-form @submit="editUser">
             <div class="d-flex align-items-center flex-column">
               <ProfileImage
-                @click="triggerInput"
-                :src="url || userData.imageUrl"
-                customClass="profile-main-picture"
-                divCustomClass="div-main-picture"
+                  @click="triggerInput"
+                  :src="url || userData.imageUrl"
+                  customClass="profile-main-picture"
+                  divCustomClass="div-main-picture"
               />
             </div>
             <b-form-group>
               <button
-                class="create-button btn-block w-50 mx-auto mb-3 d-flex align-items-center justify-content-center"
-                @click="triggerInput"
-                type="button"
-                aria-label="Change my profile picture"
+                  class="create-button btn-block w-50 mx-auto mb-3 d-flex align-items-center justify-content-center"
+                  @click="triggerInput"
+                  type="button"
+                  aria-label="Change my profile picture"
               >
                 <span class="button-text mr-2 d-none d-md-block"
-                  >Change my profile picture</span
+                >Change my profile picture</span
                 >
                 <span class="button-text mr-2 d-md-none d-lg-none d-xl-none"
-                  >Edit</span
+                >Edit</span
                 >
                 <svg
-                  style="width:24px;height:24px;margin-bottom:5px"
-                  viewBox="0 0 24 24"
+                    style="width:24px;height:24px;margin-bottom:5px"
+                    viewBox="0 0 24 24"
                 >
                   <path
-                    fill="rgba(253, 45, 6, 0.5)"
-                    d="M15.5,9C16.2,9 16.79,8.76 17.27,8.27C17.76,7.79 18,7.2 18,6.5C18,5.83 17.76,5.23 17.27,4.73C16.79,4.23 16.2,4 15.5,4C14.83,4 14.23,4.23 13.73,4.73C13.23,5.23 13,5.83 13,6.5C13,7.2 13.23,7.79 13.73,8.27C14.23,8.76 14.83,9 15.5,9M19.31,8.91L22.41,12L21,13.41L17.86,10.31C17.08,10.78 16.28,11 15.47,11C14.22,11 13.16,10.58 12.3,9.7C11.45,8.83 11,7.77 11,6.5C11,5.27 11.45,4.2 12.33,3.33C13.2,2.45 14.27,2 15.5,2C16.77,2 17.83,2.45 18.7,3.33C19.58,4.2 20,5.27 20,6.5C20,7.33 19.78,8.13 19.31,8.91M16.5,18H5.5L8.25,14.5L10.22,16.83L12.94,13.31L16.5,18M18,13L20,15V20C20,20.55 19.81,21 19.41,21.4C19,21.79 18.53,22 18,22H4C3.45,22 3,21.79 2.6,21.4C2.21,21 2,20.55 2,20V6C2,5.47 2.21,5 2.6,4.59C3,4.19 3.45,4 4,4H9.5C9.2,4.64 9.03,5.31 9,6H4V20H18V13Z"
+                      fill="rgba(253, 45, 6, 0.5)"
+                      d="M15.5,9C16.2,9 16.79,8.76 17.27,8.27C17.76,7.79 18,7.2 18,6.5C18,5.83 17.76,5.23 17.27,4.73C16.79,4.23 16.2,4 15.5,4C14.83,4 14.23,4.23 13.73,4.73C13.23,5.23 13,5.83 13,6.5C13,7.2 13.23,7.79 13.73,8.27C14.23,8.76 14.83,9 15.5,9M19.31,8.91L22.41,12L21,13.41L17.86,10.31C17.08,10.78 16.28,11 15.47,11C14.22,11 13.16,10.58 12.3,9.7C11.45,8.83 11,7.77 11,6.5C11,5.27 11.45,4.2 12.33,3.33C13.2,2.45 14.27,2 15.5,2C16.77,2 17.83,2.45 18.7,3.33C19.58,4.2 20,5.27 20,6.5C20,7.33 19.78,8.13 19.31,8.91M16.5,18H5.5L8.25,14.5L10.22,16.83L12.94,13.31L16.5,18M18,13L20,15V20C20,20.55 19.81,21 19.41,21.4C19,21.79 18.53,22 18,22H4C3.45,22 3,21.79 2.6,21.4C2.21,21 2,20.55 2,20V6C2,5.47 2.21,5 2.6,4.59C3,4.19 3.45,4 4,4H9.5C9.2,4.64 9.03,5.31 9,6H4V20H18V13Z"
                   />
                 </svg>
               </button>
               <div class="d-flex align-items-center">
                 <b-col sm="10">
                   <input
-                    ref="fileUpload"
-                    class="d-none"
-                    type="file"
-                    @change="onFileSelected"
+                      ref="fileUpload"
+                      class="d-none"
+                      type="file"
+                      @change="onFileSelected"
                   />
                 </b-col>
               </div>
@@ -55,11 +55,11 @@
                 </b-col>
                 <b-col sm="10">
                   <b-form-input
-                    id="firstName"
-                    type="text"
-                    placeholder="First Name"
-                    v-model="input.firstName"
-                    class="text-dark mb-2 pl-lg-3"
+                      id="firstName"
+                      type="text"
+                      placeholder="First Name"
+                      v-model="input.firstName"
+                      class="text-dark mb-2 pl-lg-3"
                   ></b-form-input>
                 </b-col>
               </div>
@@ -69,11 +69,11 @@
                 </b-col>
                 <b-col sm="10">
                   <b-form-input
-                    id="lastName"
-                    type="text"
-                    placeholder="Last Name"
-                    v-model="input.lastName"
-                    class="text-dark mb-2 pl-lg-3"
+                      id="lastName"
+                      type="text"
+                      placeholder="Last Name"
+                      v-model="input.lastName"
+                      class="text-dark mb-2 pl-lg-3"
                   ></b-form-input>
                 </b-col>
               </div>
@@ -83,12 +83,12 @@
                 </b-col>
                 <b-col sm="10">
                   <b-form-input
-                    id="email"
-                    type="email"
-                    placeholder="Email"
-                    v-model="input.email"
-                    class="mb-2 pl-lg-3"
-                    disabled
+                      id="email"
+                      type="email"
+                      placeholder="Email"
+                      v-model="input.email"
+                      class="mb-2 pl-lg-3"
+                      disabled
                   ></b-form-input>
                 </b-col>
               </div>
@@ -98,20 +98,20 @@
                 </b-col>
                 <b-col sm="10">
                   <b-form-input
-                    id="admin"
-                    type="text"
-                    placeholder="Admin"
-                    class="mb-2 pl-lg-3"
-                    disabled
+                      id="admin"
+                      type="text"
+                      placeholder="Admin"
+                      class="mb-2 pl-lg-3"
+                      disabled
                   ></b-form-input>
                 </b-col>
               </div>
             </b-form-group>
             <button
-              type="submit"
-              :class="`save-btn ${emptyInput ? 'disabled' : ''}`"
-              :disabled="emptyInput"
-              aria-label="Save"
+                type="submit"
+                :class="`save-btn ${emptyInput ? 'disabled' : ''}`"
+                :disabled="emptyInput"
+                aria-label="Save"
             >
               Save
             </button>
@@ -122,7 +122,7 @@
       </b-col>
     </b-row>
 
-    <PostsList :userId="userData.userId" />
+    <PostsList :userId="userData.id" />
   </div>
 </template>
 
@@ -132,7 +132,6 @@ import ProfileImage from '../components/ProfileImage'
 import PostsList from '../components/PostsList'
 import Menu from '../components/Menu'
 import UserDeleteAccount from '../components/UserDeleteAccount'
-import { format } from 'prettier'
 
 export default {
   name: 'Profile',
@@ -146,61 +145,48 @@ export default {
     const userData = JSON.parse(localStorage.getItem('userData'))
     return {
       userData,
-      content: '',
       input: {
         firstName: userData.firstName,
         lastName: userData.lastName,
-        email: userData.email,
-        imageUrl: userData.imageUrl
+        email: userData.email
       },
       selectedFile: null,
       url: null
     }
   },
-
   methods: {
-    onFileSelected (event) {
+    onFileSelected () {
       this.url = URL.createObjectURL(event.target.files[0])
       this.selectedFile = event.target.files[0]
-      //check why event is crossed out so console.log(event.target.files[0]);
-      console.log("try events", event.target.files[0]);
     },
 
     triggerInput () {
       this.$refs.fileUpload.click()
     },
 
-    //editUser (event) {
-    editUser () {
-      
+    editUser (event) {
       let body = this.input
-      
-      //console.log("uhh", this.input);
-      
+
       const isFormData = !!this.selectedFile
-      /*REVISE THIS -194*/
+
       if (isFormData) {
         const formData = new FormData()
         formData.append('image', this.selectedFile)
         formData.append('user', JSON.stringify(body))
-        body = formData;
-
-      apiClient.put('api/auth/edit', body, { isFormData })
-      .then(res => {
-
-        //console.log("uhh", res.user);
+        body = formData
+      }
+      apiClient.put('api/auth/edit', body, { isFormData }).then(res => {
         localStorage.setItem('userData', JSON.stringify(res.user))
         this.userData = res.user
-        //window.location.reload()
+        window.location.reload()
       })
-      }
     }
   },
   computed: {
     emptyInput () {
       return (
-        !this.input.firstName.trim().length ||
-        !this.input.lastName.trim().length
+          !this.input.firstName.trim().length ||
+          !this.input.lastName.trim().length
       )
     }
   }
